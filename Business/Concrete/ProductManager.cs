9e58d66ef588a3a3267371bc1cadc2cd1730780a
Ahmet AKDEMIR <ahmet.akdemir@st.uskudar.edu.tr>
2022-10-06 09:46:56 +0300

    05.10

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,10 +24,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour==4)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
             //Business codes
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
 
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,7 +11,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Product is added!";
         public static string ProductNameInvalid = "Product name is invalid!";
-        internal static string MaintenanceTime = "System is in MaintenanceTime";
-        internal static string ProductsListed = "Products are listed!";
+        public static string MaintenanceTime = "System is in MaintenanceTime";
+        public static string ProductsListed = "Products are listed!";
     }
 }
